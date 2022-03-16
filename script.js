fce24cd0d9488b372d3791d3456f22c9274b0ac3
Gabriel <gscribot@outlook.fr>
2022-03-16 02:04:36 +0100

    site fini avant texte

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ $(document).ready(function (){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
 
     //toggle menu/navbar script
@@ -12,6 +17,22 @@ $(document).ready(function (){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+      strings: ["Webdesigner", "Communicant", "Artiste"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Webdesigner", "Communicant", "Artiste"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+
 
     // owl carousel script
     $('.carousel').owlCarousel({
